f663ddf8e65518c15631e887998265ac5188b9fe
Paul Hasfjord <paul.hasfjord@adera.no>
2020-09-04 06:50:11 +0200

    changed main with two new methods checking nullpointer java 14

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
+       objectNullpointer();
+       stringNullpointer();
+    }
+
+    private static void objectNullpointer(){
         Object myObject = null;
         myObject.toString().hashCode();
+    }
+    private static void stringNullpointer(){
+        String nullValue = null;
+        nullValue.toString().length();
     }
 }
